bd15b3007f8e23e0d848d4d873e309d8c8ddc717
maysa matos <maysamatos@risestudio.com.br>
2025-04-02 13:38:39 -0300

    vscode

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,35 +3,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 type Resposta = {
   orderId: string
 }
-type PurchasePayload = {
-  products: [
-    {
-      id: number
-      price: number
-    }
-  ]
-  delivery: {
-    receiver: string
-    address: {
-      description: string
-      city: string
-      zipCode: string
-      number: number
-      complement?: string
-    }
-  }
-  payment: {
-    card: {
-      name: string
-      number: string
-      code: number
-      expires: {
-        month: number
-        year: number
-      }
-    }
-  }
-}
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
